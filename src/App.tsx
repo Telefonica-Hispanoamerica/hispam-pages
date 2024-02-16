e9ea630d097504e090876109063d9bd8ff1ea9ec
Michael Reyes <maaaik.dev@gmail.com>
2024-02-16 09:30:37 -0500

    inSignIn func

--- src/App.tsx
+++ src/App.tsx
@@ -89,6 +89,14 @@ function App() {
 	};
 
 	// Google Analytics	
+
+	function onSignIn(googleUser: any) {
+		var profile = googleUser.getBasicProfile();
+		console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+		console.log('Name: ' + profile.getName());
+		console.log('Image URL: ' + profile.getImageUrl());
+		console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+	}
 	
 
 	return (	
@@ -165,6 +173,7 @@ function App() {
 			<GoogleOAuthProvider clientId={ clientId }>
 				<GoogleAnalyticsApp />
 			</GoogleOAuthProvider>
+			{/* <div className="g-signin2" data-onsuccess="onSignIn"></div> */}
 			{/* <AnalyticsPage /> */}
 		</div>		
 	)
